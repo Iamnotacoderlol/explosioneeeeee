47dd82b29b4081d5884129c505757237c1b6239a
Iamnotacoderlol <99046187+Iamnotacoderlol@users.noreply.github.com>
2022-02-04 11:22:43 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onSound(DetectedSound.Loud, function () {
+	
+})
+let explosion_sound: number[] = []
+music.startMelody(explosion_sound, MelodyOptions.Once)
+basic.forever(function () {
+	
+})
